a1a9acf23fc283ee697270f3d3adc64e7b517e0c
Vasyl <fomvasss@gmail.com>
2024-12-12 17:34:42 +0200

    add to xEditable disabled

--- resources/views/components/xEditable.blade.php
+++ resources/views/components/xEditable.blade.php
@@ -4,17 +4,23 @@
     $title = ($limit = \Illuminate\Support\Arr::get($attrs, 'limit_title')) ? \Illuminate\Support\Str::limit($title, $limit, $limitEnd) : $title;
 @endphp
 
-<a @empty(Arr::get($attrs, 'disabled')) href="#" @endempty
-   class="f-x-editable {{ $attrs['class'] ?? null }} {{ $attrs['class_wrap'] ?? null }}"
+<a @if(Arr::get($attrs, 'disabled') || Arr::get($attrs, 'readonly')) href="#" @endif
+class="f-x-editable {{ $attrs['class'] ?? null }} {{ $attrs['class_wrap'] ?? null }}"
    data-value="{{ $value }}"
-   data-type="{{ $attrs['data_type'] ?? $attrs['type'] ?? 'text' }}"
-   data-name="{{ $name }}"
-   @if(Arr::get($attrs, 'disabled')) data-disabled="1" @endif
-   @isset($attrs['mode']) data-mode="{{$attrs['type']}}" @endisset
-   @isset($attrs['inputclass'])data-mode="{{$attrs['inputclass']}}" @endisset
-   @isset($attrs['viewformat'])data-viewformat="{{$attrs['viewformat']}}" @endisset
-   @isset($attrs['source'])data-source="{{ json_encode($attrs['source']) }}" @endisset
-   @isset($attrs['url_save'])data-url="{{ $attrs['url_save'] }}" @endisset
-   data-pk="@isset($attrs['pk']){{$attrs['pk']}}@else{{\Illuminate\Support\Str::random()}}@endisset"
+@foreach(Arr::only($attrs, $field_attrs) as $key => $val)
+    {{$key}}="{{$val}}"
+@endforeach
+@foreach(Arr::get($attrs, 'attrs') ?? [] as $key => $val)
+    {{$key}}="{{$val}}"
+@endforeach
+data-type="{{ $attrs['data_type'] ?? $attrs['type'] ?? 'text' }}"
+data-name="{{ $name }}"
+@if(Arr::get($attrs, 'disabled') || Arr::get($attrs, 'readonly')) data-disabled="1" @endif
+@isset($attrs['mode']) data-mode="{{$attrs['type']}}" @endisset
+@isset($attrs['inputclass'])data-mode="{{$attrs['inputclass']}}" @endisset
+@isset($attrs['viewformat'])data-viewformat="{{$attrs['viewformat']}}" @endisset
+@isset($attrs['source'])data-source="{{ json_encode($attrs['source']) }}" @endisset
+@isset($attrs['url_save'])data-url="{{ $attrs['url_save'] }}" @endisset
+data-pk="@isset($attrs['pk']){{$attrs['pk']}}@else{{\Illuminate\Support\Str::random()}}@endisset"
 > {{ $title }}
 </a>
