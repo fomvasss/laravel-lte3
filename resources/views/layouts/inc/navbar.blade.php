e5f41e89edbc71127198975a3c27ed3c740dad86
Vasyl <fomvasss@gmail.com>
2025-06-24 14:01:12 +0300

    upd navbar height

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand {{config('lte3.view.dark_mode') ? 'dark-mode' : 'navbar-white navbar-light'}}">
+<nav class="main-header navbar navbar-expand text-sm {{config('lte3.view.dark_mode') ? 'dark-mode' : 'navbar-white navbar-light'}}">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
 
-    <a href="{{ url(config('lte3.dashboard_slug')) }}" class="brand-link">
+    <a href="{{ url(config('lte3.dashboard_slug')) }}" class="brand-link text-sm">
       <img src="/vendor/adminlte/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">{!! config('lte3.logo') !!}</span>
     </a>
